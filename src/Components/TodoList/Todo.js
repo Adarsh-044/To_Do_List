4ba7ce126f1cd8b64651e3a7c11f6e443106bde6
Adarsh <adarshxy123@gmail.com>
2022-06-02 17:58:10 +0530

    Button functioning

--- src/Components/TodoList/Todo.js
+++ src/Components/TodoList/Todo.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import "./style.css"
 
 const Todo = () => {
@@ -6,14 +6,32 @@ const Todo = () => {
   const [inputData, setInputData] = useState("");   // To get the data from the user. 
   const [items, setItems] = useState([]);
 
+  // Add the item function 
   const addItem = () => {
     if (!inputData)
       alert('Please fill the data');
     else {
-      setItems([...items, inputData]);
+      const myNewInputData = {
+        id: new Date().getTime().toString(),
+        name: inputData,
+      }
+      setItems([...items, myNewInputData]);
       setInputData("");
     }
   };
+
+  // Delete function 
+  const deleteItem = (id) => {
+    const updatedItems = items.filter((currEle) => {
+      return currEle.id !== id;
+    });
+    setItems(updatedItems);
+  }
+
+  // Remove all elements function 
+  const removeAll = () => {
+    setItems([]);
+  }
   return (
     <>
       <div className="main-div">
@@ -39,24 +57,20 @@ const Todo = () => {
 
             {items.map((currEle, idx) => {
               return (
-                <div className="eachItem" key={idx}>
-                  <h3>{idx + 1} .  {currEle}</h3>
+                <div className="eachItem" key={currEle.id}>
+                  <h3>{idx + 1} .  {currEle.name}</h3>
                   <div className="todo-btn">
                     <i className="fas fa-edit add-btn" ></i>
-                    <i className="far fa-trash-alt add-btn"></i>
+                    <i className="far fa-trash-alt add-btn" onClick={() => deleteItem(currEle.id)} ></i>
                   </div>
                 </div>
               )
             })}
-
-
-
-
           </div>
 
           {/* Remove All */}
           <div className="showItems">
-            <button className='btn effect04' data-sm-link-text="Remove All">
+            <button className='btn effect04' data-sm-link-text="Remove All" onClick={removeAll}>
               <span>
                 CHECK LIST
               </span>
