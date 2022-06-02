8eca2af1030c930f9ff5dfd3eec916f587fc5194
Adarsh <adarshxy123@gmail.com>
2022-06-02 12:31:26 +0530

    Addtion of items in main each item completed

--- src/Components/TodoList/Todo.js
+++ src/Components/TodoList/Todo.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./style.css"
 
 const Todo = () => {
+
+  const [inputData, setInputData] = useState("");   // To get the data from the user. 
+  const [items, setItems] = useState([]);
+
+  const addItem = () => {
+    if (!inputData)
+      alert('Please fill the data');
+    else {
+      setItems([...items, inputData]);
+      setInputData("");
+    }
+  };
   return (
     <>
       <div className="main-div">
@@ -12,18 +24,34 @@ const Todo = () => {
             <figcaption>Add Your List Here ✌</figcaption>
           </figure>
           <div className="addItems" >
-            <input type="text" placeholder='✍ Add Item' className='form-control' />
-            <i className="fa fa-plus"></i>
+            <input type="text"
+              placeholder='✍ Add Item'
+              className='form-control'
+              value={inputData}
+              onChange={(event) => {
+                setInputData(event.target.value)
+              }} />
+
+            <i className="fa fa-plus" onClick={addItem}></i>
           </div>
           {/* Show Items */}
           <div className="showItems">
-            <div className="eachItem">
-              <h3>Apple</h3>
-              <div className="todo-btn">
-                <i className="fas fa-edit add-btn"></i>
-                <i className="far fa-trash-alt add-btn"></i>
-              </div>
-            </div>
+
+            {items.map((currEle, idx) => {
+              return (
+                <div className="eachItem" key={idx}>
+                  <h3>{idx + 1} .  {currEle}</h3>
+                  <div className="todo-btn">
+                    <i className="fas fa-edit add-btn" ></i>
+                    <i className="far fa-trash-alt add-btn"></i>
+                  </div>
+                </div>
+              )
+            })}
+
+
+
+
           </div>
 
           {/* Remove All */}
