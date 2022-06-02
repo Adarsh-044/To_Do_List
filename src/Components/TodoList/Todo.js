1aaa20c1fea23e014b475d115feac6bbb5d6b57b
Adarsh <adarshxy123@gmail.com>
2022-06-02 19:38:21 +0530

    Edit function

--- src/Components/TodoList/Todo.js
+++ src/Components/TodoList/Todo.js
@@ -15,17 +15,44 @@ const Todo = () => {
 
   const [inputData, setInputData] = useState("");   // To get the data from the user. 
   const [items, setItems] = useState(getLocalData());
+  const [isEditItem, setIsEditItem] = useState("");
+  const [ToggleButton, setToggleButton] = useState(false);
+
   // Add the item function 
   const addItem = () => {
     if (!inputData)
       alert('Please fill the data');
+    else if (inputData && ToggleButton) {
+      setItems(
+        items.map((currEle) => {
+          if (currEle.id === isEditItem) {
+            return { ...currEle, name: inputData };
+          };
+          return currEle;
+        })
+      )
+      setInputData([]);
+      setIsEditItem(null);
+      setToggleButton(false);
+    }
     else {
-      const myNewInputData = {
-        id: new Date().getTime().toString(),
-        name: inputData,
+      let adder = true;
+      items.map((currEle) => {
+        if (currEle.name === inputData) {
+          alert('Enter another data');
+          adder = false;
+        };
+        return currEle;
+      })
+
+      if (adder) {
+        const myNewInputData = {
+          id: new Date().getTime().toString(),
+          name: inputData,
+        }
+        setItems([...items, myNewInputData]);
+        setInputData("");
       }
-      setItems([...items, myNewInputData]);
-      setInputData("");
     }
   };
 
@@ -48,6 +75,15 @@ const Todo = () => {
     localStorage.setItem("mytodolist", JSON.stringify(items));
   }, [items]);
 
+  // Edit items
+  const editItem = (idx) => {
+    const item_todo_edited = items.find((currEle) => {
+      return currEle.id === idx;
+    });
+    setInputData(item_todo_edited.name);
+    setIsEditItem(idx);
+    setToggleButton(true);
+  }
 
   return (
     <>
@@ -66,8 +102,8 @@ const Todo = () => {
               onChange={(event) => {
                 setInputData(event.target.value)
               }} />
-
-            <i className="fa fa-plus" onClick={addItem}></i>
+            {ToggleButton ? <i className="fas fa-edit add-btn" onClick={addItem}></i> : <i className="fa fa-plus" onClick={addItem}></i>
+            }
           </div>
           {/* Show Items */}
           <div className="showItems">
@@ -77,7 +113,7 @@ const Todo = () => {
                 <div className="eachItem" key={currEle.id}>
                   <h3>{idx + 1} .  {currEle.name}</h3>
                   <div className="todo-btn">
-                    <i className="fas fa-edit add-btn" ></i>
+                    <i className="fas fa-edit add-btn" onClick={() => { editItem(currEle.id) }} ></i>
                     <i className="far fa-trash-alt add-btn" onClick={() => deleteItem(currEle.id)} ></i>
                   </div>
                 </div>
